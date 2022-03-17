5daf4d7361b1f91f2d08955f61e2288752067941
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-03-17 17:39:03 +0300

    init

---
+++ mod/src/main/kotlin/App.kt
@@ -0,0 +1,120 @@
+import dev.xdark.clientapi.event.lifecycle.GameLoop
+import dev.xdark.clientapi.event.render.RenderPass
+import dev.xdark.clientapi.opengl.GlStateManager
+import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL11.GL_POLYGON
+import org.lwjgl.opengl.GL11.glCallList
+import ru.cristalix.clientapi.KotlinMod
+import ru.cristalix.uiengine.UIEngine
+import ru.cristalix.uiengine.utility.V3
+import kotlin.math.cos
+import kotlin.math.sin
+
+lateinit var mod: App
+
+class App : KotlinMod() {
+
+    lateinit var cube: V3
+    var inited = false
+
+    override fun onEnable() {
+        mod = this
+        UIEngine.initialize(this)
+
+        registerChannel("tower:init") {
+            cube = V3(
+                readDouble(),
+                readDouble() + 1,
+                readDouble()
+            )
+            inited = true
+        }
+
+        var angle = 0.0
+        var last = System.currentTimeMillis()
+
+
+        registerHandler<GameLoop> {
+            val computed = angle / 20.0
+            if (System.currentTimeMillis() - last > 200) {
+                last = System.currentTimeMillis()
+            }
+        }
+
+        registerHandler<RenderPass> {
+            if (inited) {
+                angle = if (angle > 7200) 0.0 else angle + 1
+
+                val player = clientApi.minecraft().player
+
+                // GL начало
+                GlStateManager.disableLighting()
+                GlStateManager.disableTexture2D()
+                GlStateManager.disableAlpha()
+                GlStateManager.disableCull()
+                GlStateManager.shadeModel(GL11.GL_SMOOTH)
+                GlStateManager.enableBlend()
+                GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_CONSTANT_COLOR)
+                GlStateManager.color(
+                    255f,
+                    255f,
+                    255f,
+                    0.6f
+                )
+
+                GlStateManager.translate(-player.x, -player.y, -player.z)
+                GlStateManager.translate(cube.x, cube.y, cube.z)
+                GlStateManager.rotate(
+                    (angle / 23.0f).toFloat(),
+                    cos(angle / 31.0).toFloat(),
+                    sin(angle / 47.0).toFloat(),
+                    sin(angle / 51.0).toFloat()
+                )
+
+                GL11.glBegin(GL_POLYGON)
+                GL11.glColor3d(1.0, 1.0, 1.0)
+                GL11.glVertex3d(0.5, -0.5, 0.5)
+                GL11.glVertex3d(0.5, 0.5, 0.5)
+                GL11.glVertex3d(-0.5, 0.5, 0.5)
+                GL11.glVertex3d(-0.5, -0.5, 0.5)
+                GL11.glEnd()
+                GL11.glBegin(GL_POLYGON)
+                GL11.glColor3d(1.0, 0.0, 1.0)
+                GL11.glVertex3d(0.5, -0.5, -0.5)
+                GL11.glVertex3d(0.5, 0.5, -0.5)
+                GL11.glVertex3d(0.5, 0.5, 0.5)
+                GL11.glVertex3d(0.5, -0.5, 0.5)
+                GL11.glEnd()
+                GL11.glBegin(GL_POLYGON)
+                GL11.glColor3d(0.0, 1.0, 0.0)
+                GL11.glVertex3d(-0.5, -0.5, 0.5)
+                GL11.glVertex3d(-0.5, 0.5, 0.5)
+                GL11.glVertex3d(-0.5, 0.5, -0.5)
+                GL11.glVertex3d(-0.5, -0.5, -0.5)
+                GL11.glEnd()
+                GL11.glBegin(GL_POLYGON)
+                GL11.glColor3d(0.0, 0.0, 1.0)
+                GL11.glVertex3d(0.5, 0.5, 0.5)
+                GL11.glVertex3d(0.5, 0.5, -0.5)
+                GL11.glVertex3d(-0.5, 0.5, -0.5)
+                GL11.glVertex3d(-0.5, 0.5, 0.5)
+                GL11.glEnd()
+                GL11.glBegin(GL_POLYGON)
+                GL11.glColor3d(1.0, 0.0, 0.0)
+                GL11.glVertex3d(0.5, -0.5, -0.5)
+                GL11.glVertex3d(0.5, -0.5, 0.5)
+                GL11.glVertex3d(-0.5, -0.5, 0.5)
+                GL11.glVertex3d(-0.5, -0.5, -0.5)
+                GL11.glEnd()
+
+                // GL конец
+                GlStateManager.color(1f, 1f, 1f, 1f)
+                GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_CONSTANT_COLOR)
+                GlStateManager.shadeModel(GL11.GL_FLAT)
+                GlStateManager.enableTexture2D()
+                GlStateManager.enableAlpha()
+                GlStateManager.enableCull()
+            }
+        }
+    }
+}
---
+++ node/src/main/kotlin/me.func.tower/App.kt
@@ -0,0 +1,88 @@
+package me.func.tower
+
+import dev.implario.bukkit.platform.Platforms
+import dev.implario.bukkit.world.Label
+import dev.implario.games5e.node.CoordinatorClient
+import dev.implario.games5e.node.NoopGameNode
+import dev.implario.games5e.sdk.cristalix.MapLoader
+import dev.implario.games5e.sdk.cristalix.WorldMeta
+import dev.implario.platform.impl.darkpaper.PlatformDarkPaper
+import me.func.mod.Anime
+import me.func.mod.Kit
+import me.func.mod.conversation.ModLoader
+import me.func.mod.conversation.ModTransfer
+import net.minecraft.server.v1_12_R1.MinecraftServer
+import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.java.JavaPlugin
+import ru.cristalix.core.CoreApi
+import ru.cristalix.core.network.ISocketClient
+import ru.cristalix.core.party.IPartyService
+import ru.cristalix.core.party.PartyService
+import ru.cristalix.core.realm.IRealmService
+import ru.cristalix.core.realm.RealmStatus
+import ru.cristalix.core.transfer.ITransferService
+import ru.cristalix.core.transfer.TransferService
+
+lateinit var app: App
+
+class App : JavaPlugin(), Listener {
+
+    val map = WorldMeta(MapLoader.load("func", "tower"))
+    val client = CoordinatorClient(NoopGameNode())
+    val spawn: Label = map.getLabel("spawn").apply {
+        x += 0.5
+        z += 0.5
+        yaw = -90f
+    }
+    val tower = map.getLabel("tower").apply {
+        x += 0.5
+        z += 0.5
+    }
+    val generators = map.getLabels("mob")
+
+    override fun onEnable() {
+        app = this
+
+        CoreApi.get().apply {
+            registerService(ITransferService::class.java, TransferService(this.socketClient))
+            registerService(IPartyService::class.java, PartyService(ISocketClient.get()))
+        }
+
+        Platforms.set(PlatformDarkPaper())
+
+        Anime.include(Kit.STANDARD, Kit.EXPERIMENTAL, Kit.DIALOG, Kit.NPC)
+        ModLoader.loadAll("mods")
+
+        IRealmService.get().currentRealmInfo.apply {
+            status = RealmStatus.WAITING_FOR_PLAYERS
+            isLobbyServer = true
+            readableName = "Просто снос башки"
+            groupName = "Секрет"
+            servicedServers = arrayOf("SEC")
+        }
+
+        Bukkit.getPluginManager().apply {
+            registerEvents(this@App, this@App)
+        }
+    }
+
+    @EventHandler
+    fun PlayerJoinEvent.handle() = player.apply {
+        teleport(spawn)
+        MinecraftServer.SERVER.postToNextTick {
+            // Отправляем наш мод
+            ModLoader.send("tower-mod-bundle.jar", player)
+
+            // Отправляем точку башни
+            ModTransfer(tower.x, tower.y, tower.z).send("tower:init", player)
+
+            // Отправляем точки со спавнерами
+            val mobs = ModTransfer().integer(generators.size)
+            generators.forEach { mobs.double(it.x).double(it.y).double(it.z) }
+            mobs.send("mobs:init", player)
+        }
+    }
+}
