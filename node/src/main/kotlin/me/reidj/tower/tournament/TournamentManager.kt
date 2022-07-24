32c35d8aa6c358d43a8f1c94a28cb004d5e9258e
reidjJava <sp1.ke@mail.ru>
2022-07-24 21:43:19 +0300

    Update TournamentManager.kt

--- node/src/main/kotlin/me/reidj/tower/tournament/TournamentManager.kt
+++ node/src/main/kotlin/me/reidj/tower/tournament/TournamentManager.kt
@@ -3,16 +3,16 @@ package me.reidj.tower.tournament
 import me.reidj.tower.app
 import me.reidj.tower.user.User
 import org.bukkit.Bukkit
-import java.time.LocalTime
+import java.time.*
 import java.time.temporal.ChronoUnit
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 object TournamentManager {
 
-    private const val COUNTDOWN_DAYS = 3
+    private const val COUNTDOWN_DAYS = 3L
 
-    private val startDate = Date(2022, 7, 2)
+    val startDate = LocalDate.of(2022, 7, 2)
 
     fun end(user: User) = user.run {
         tournament.wavePassed.add(user.wave!!.level)
@@ -22,16 +22,20 @@ object TournamentManager {
     fun getOnlinePlayers() = Bukkit.getOnlinePlayers().mapNotNull { app.getUser(it) }.filter { it.inGame }
 
     fun isTournamentDay() =
-        TimeUnit.DAYS.convert(System.currentTimeMillis() - startDate.time, TimeUnit.MILLISECONDS) % COUNTDOWN_DAYS == 0L
+        TimeUnit.DAYS.convert(
+            System.currentTimeMillis() - startDate.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli(),
+            TimeUnit.MILLISECONDS
+        ) % COUNTDOWN_DAYS == 0L
 
-    fun getTimeBefore(unit: TimeUnit): Long {
-        if (isTournamentDay()) return 0
-        var nextTournament = startDate.time
-        val now = System.currentTimeMillis()
-        while (nextTournament < now) {
-            nextTournament += COUNTDOWN_DAYS * 86_400 * 1_000
+    fun getTimeBefore(): Duration {
+        if (isTournamentDay()) return Duration.ZERO
+        var duration = Duration.ZERO
+        var i = 1
+        while (duration.isNegative || duration.isZero) {
+            duration = Duration.between(LocalDateTime.now(), startDate.plusDays(COUNTDOWN_DAYS * i).atStartOfDay())
+            i++
         }
-        return unit.convert(nextTournament - now, TimeUnit.MILLISECONDS)
+        return duration
     }
 
     fun getTimeAfter(unit: ChronoUnit) = unit.between(LocalTime.now(), LocalTime.MAX)
